90762dc1870a43f3a7d8579b1496dc6ace46ebdb
rubigc <111653095+rubigc@users.noreply.github.com>
2025-12-04 11:46:38 +0000

    day2

---
+++ 2025/2/silver.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	dat, err := os.ReadFile("input.txt")
+	check(err)
+	data := strings.Split(string(dat), ",")
+	foundNum :=0
+	for _, x := range data {
+		var numbersInt []int
+		numbersStr := strings.Split(x, "-")
+
+		tempVar, _  := strconv.Atoi(numbersStr[0])
+		numbersInt = append(numbersInt, tempVar)
+		tempVar, _ = strconv.Atoi(numbersStr[1])
+		numbersInt = append(numbersInt, tempVar)
+		
+		counter := numbersInt[1] - numbersInt[0]
+
+		for counter > -1 {
+			tempNum := strconv.Itoa(numbersInt[1]-counter)
+			strLen := len(tempNum)
+			
+			if(tempNum[strLen/2:] == tempNum[:strLen/2]) {
+				y, _ := strconv.Atoi(tempNum)
+				foundNum += y
+			}
+			counter--
+		}
+
+	}
+	fmt.Println(foundNum)
+}
